221dffdf31ccf2538621bac2b87e2487627c59ad
Mackenzie Wright <mackenziewright@u.boisestate.edu>
2026-05-11 00:34:23 -0600

    extra credit

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -33,9 +35,6 @@ public class GettingStartedApplication {
             final var statement = connection.createStatement();
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(50))");
 
-            String randomString = getRandomString();
-            statement.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + randomString + "')");
-
             final var resultSet = statement.executeQuery("SELECT tick, random_string FROM table_timestamp_and_random_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
@@ -50,6 +49,22 @@ public class GettingStartedApplication {
             model.put("message", t.getMessage());
             return "error";
         }
+    }
+
+    @GetMapping("/dbinput")
+    public String dbinputForm() {
+        return "dbinput";
+    }
+
+    @PostMapping("/dbinput")
+    public String dbinputSubmit(@RequestParam String inputString) {
+        try (Connection connection = dataSource.getConnection()) {
+            final var statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO table_timestamp_and_random_string (tick, random_string) VALUES (now(), '" + inputString + "')");
+        } catch (Throwable t) {
+            // Handle the exception
+        }
+        return "redirect:/database";
     }
 
     private String getRandomString() {
